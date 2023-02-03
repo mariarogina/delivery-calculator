2d7723c82112487a512256ee5768d626b02e669d
Mariia <whitesheepa@gmail.com>
2023-02-04 01:01:56 +0200

    max fee and free delivery

--- src/logic/calculator.test.ts
+++ src/logic/calculator.test.ts
@@ -23,6 +23,7 @@ describe("calculator", () => {
         time: new Date(),
       })
     ).toStrictEqual(null);
+    //incorrect product amount only
     expect(
       calculateDeliveryPrice({
         cartValue: 10,
@@ -31,6 +32,7 @@ describe("calculator", () => {
         time: new Date(),
       })
     ).toStrictEqual(null);
+    //incorrect cart value only
     expect(
       calculateDeliveryPrice({
         cartValue: -10,
@@ -41,8 +43,6 @@ describe("calculator", () => {
     ).toStrictEqual(null);
   });
 
- 
-
   it("cart value", () => {
     expect(
       calculateDeliveryPrice({
@@ -52,7 +52,84 @@ describe("calculator", () => {
         time: new Date("2022-03-02T00:02:02.000Z"),
       })
     ).toStrictEqual(2);
+    expect(
+      calculateDeliveryPrice({
+        cartValue: 15,
+        distanceMeters: 1001,
+        itemsAmount: 2,
+        time: new Date("2022-03-02T00:02:02.000Z"),
+      })
+    ).toStrictEqual(3);
+    expect(
+      calculateDeliveryPrice({
+        cartValue: 15,
+        distanceMeters: 1499,
+        itemsAmount: 2,
+        time: new Date("2022-03-02T00:02:02.000Z"),
+      })
+    ).toStrictEqual(3);
+    expect(
+      calculateDeliveryPrice({
+        cartValue: 15,
+        distanceMeters: 1501,
+        itemsAmount: 2,
+        time: new Date("2022-03-02T00:02:02.000Z"),
+      })
+    ).toStrictEqual(4);
   });
 });
+
+
+describe("15 eur is maximum fee", () => {
+   it("returns 15", () => {
+    expect(
+      calculateDeliveryPrice({
+        cartValue: 20,
+        distanceMeters: 9000,
+        itemsAmount: 4,
+        time: new Date("2022-03-02T00:02:02.000Z"),
+      })
+    ).toStrictEqual(15);
+  });
+})
+
+// describe("max fee", () => {
+//   it("cart value", () => {
+//     expect(
+//       calculateDeliveryPrice({
+//         cartValue: 15,
+//         distanceMeters: 1000,
+//         itemsAmount: 2,
+//         time: new Date("2022-03-02T00:02:02.000Z"),
+//       })
+//     ).toStrictEqual(2);
+//   });
+// })
+
+describe("free delivery", () => {
+  it("cart value > 100 means free delivery", () => {
+        expect(
+          calculateDeliveryPrice({
+            cartValue: 101,
+            distanceMeters: 1000,
+            itemsAmount: 2,
+            time: new Date("2022-03-02T00:02:02.000Z"),
+          })
+        ).toStrictEqual(0);
+      });
+})
+
+// describe("friday", () => {
+  // it("cart value", () => {
+    //     expect(
+    //       calculateDeliveryPrice({
+    //         cartValue: 15,
+    //         distanceMeters: 1000,
+    //         itemsAmount: 2,
+    //         time: new Date("2022-03-02T00:02:02.000Z"),
+    //       })
+    //     ).toStrictEqual(2);
+    //   });
+// })
 
 export {};
--- src/logic/calculator.ts
+++ src/logic/calculator.ts
@@ -1,16 +1,50 @@
 import { calculatorInput } from "./definitions";
 
 export function calculateDeliveryPrice(a: calculatorInput): number | null {
-  if (
-    a.distanceMeters <= 0 ||
-    a.cartValue < 0 ||
-    a.itemsAmount == 0 
-  ) {
-    return null;
-  } else if (a.distanceMeters <= 1000) {
-    return 2;
-  } else {
-    return 12345
+  let total_sum: number = 0;
+
+  //calculate distance addition
+  function calculateDistance(): number | null {
+    if (a.distanceMeters <= 0 || a.cartValue < 0 || a.itemsAmount == 0) {
+      return null;
+    } else if (a.distanceMeters <= 1000) {
+      return 2;
+    } else if (a.distanceMeters > 1000) {
+      let result = 2 + Math.ceil((a.distanceMeters - 1000) / 500);
+      return result;
+    } else {
+      return 12345;
+    }
   }
 
+  let distance: number | null = calculateDistance();
+
+  //calculate items amount
+  function calculateItems(): number | null {
+    return 0;
+  }
+
+  let items_amount: number | null = calculateItems();
+
+
+  
+
+  //calculate Friday rush
+
+//free delivery for more than 100 eur order
+  if (a.cartValue > 100) {
+    return 0;
+  } else {
+    if (distance) {
+      let result = total_sum + distance;
+
+        // max delivery 15
+        if (result > 15){
+            return 15
+        }
+      return result;
+    } else {
+      return null;
+    }
+  }
 }
