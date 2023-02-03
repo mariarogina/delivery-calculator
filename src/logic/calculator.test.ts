423ce14ceb16fcfdfd3c90c95f6d5ea1ff2026bd
Mariia <whitesheepa@gmail.com>
2023-02-03 23:11:12 +0200

    wrong input and short distance tests

--- src/logic/calculator.test.ts
+++ src/logic/calculator.test.ts
@@ -1,7 +1,7 @@
 import { calculateDeliveryPrice } from "./calculator";
 
 describe("calculator", () => {
-  it("wrong data", () => {
+  it("wrong all data", () => {
     expect(
       calculateDeliveryPrice({
         cartValue: 0,
@@ -10,6 +10,48 @@ describe("calculator", () => {
         time: new Date(),
       })
     ).toStrictEqual(null);
+  });
+
+  //incorrect distance test only
+
+  it("separate wrong values", () => {
+    expect(
+      calculateDeliveryPrice({
+        cartValue: 10,
+        distanceMeters: 0,
+        itemsAmount: 10,
+        time: new Date(),
+      })
+    ).toStrictEqual(null);
+    expect(
+      calculateDeliveryPrice({
+        cartValue: 10,
+        distanceMeters: 10,
+        itemsAmount: 0,
+        time: new Date(),
+      })
+    ).toStrictEqual(null);
+    expect(
+      calculateDeliveryPrice({
+        cartValue: -10,
+        distanceMeters: 10,
+        itemsAmount: 2,
+        time: new Date(),
+      })
+    ).toStrictEqual(null);
+  });
+
+ 
+
+  it("cart value", () => {
+    expect(
+      calculateDeliveryPrice({
+        cartValue: 15,
+        distanceMeters: 1000,
+        itemsAmount: 2,
+        time: new Date("2022-03-02T00:02:02.000Z"),
+      })
+    ).toStrictEqual(2);
   });
 });
 
--- src/logic/calculator.ts
+++ src/logic/calculator.ts
@@ -1,15 +1,16 @@
 import { calculatorInput } from "./definitions";
 
 export function calculateDeliveryPrice(a: calculatorInput): number | null {
+  if (
+    a.distanceMeters <= 0 ||
+    a.cartValue < 0 ||
+    a.itemsAmount == 0 
+  ) {
+    return null;
+  } else if (a.distanceMeters <= 1000) {
+    return 2;
+  } else {
+    return 12345
+  }
 
-    if(true){
-
-    } else if (false) {
-
-    } else {
-
-    }
-
-    
-  return null;
 }
